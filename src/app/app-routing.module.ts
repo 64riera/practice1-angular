1aa9609dfa693fdf295588e3c080c35d36228cb8
Sergio R <nokiacorporation1@gmail.com>
2019-12-08 08:54:39 -0600

    Navigation and routing working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FrutasComponent } from './frutas/frutas.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ErrorComponent } from './error/error.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'home/:nombre', component: HomeComponent },
+  { path: 'frutas', component: FrutasComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FrutasComponent } from './frutas/frutas.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FrutasComponent,
+    ProductosComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
