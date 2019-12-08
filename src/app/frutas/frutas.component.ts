40bc97ed7b4f7b52e583f5319b5801d6fcce1cf9
Sergio R <nokiacorporation1@gmail.com>
2019-12-08 09:20:42 -0600

    Fruits section ended

--- src/app/frutas/frutas.component.ts
+++ src/app/frutas/frutas.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrutasComponent implements OnInit {
 
+  public frutas: any[] = [];
+  public nombre: string = null;
+  public cantidad: number = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  agregarFruta(){
+    if( !this.nombre || !this.cantidad ){
+      alert('Ingrese datos válidos para continuar');
+      return 
+    } else {
+      this.frutas.push(
+        { nombre: this.nombre, cantidad: this.cantidad }
+      );
+      this.nombre = null;
+      this.cantidad = null;
+    }
+  }
+
+  eliminarFruta(i: number){
+    this.frutas.splice(i, 1);
   }
 
 }
